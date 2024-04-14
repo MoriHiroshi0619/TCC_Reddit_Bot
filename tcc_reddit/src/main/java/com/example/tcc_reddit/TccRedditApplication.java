b38e064ada1aef5213a06e9d656e052a91e99f8a
MoriHiroshi0619 <mori.hiroshi.a.c@gmail.com>
2024-04-13 22:25:13 -0400

    criado a aplicação java atraves do framework spring com primeiras deperencias instaladas

---
+++ tcc_reddit/src/main/java/com/example/tcc_reddit/TccRedditApplication.java
@@ -0,0 +1,13 @@
+package com.example.tcc_reddit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TccRedditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TccRedditApplication.class, args);
+	}
+
+}
---
+++ tcc_reddit/src/test/java/com/example/tcc_reddit/TccRedditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tcc_reddit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TccRedditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
