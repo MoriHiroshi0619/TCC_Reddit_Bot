f2c827385bb5028ff2490012b59cac9bbcc75ac0
MoriHiroshi0619 <mori.hiroshi.a.c@gmail.com>
2024-04-22 00:09:43 -0400

    implentação de alguns metodos no controller e melhoria na classe Credentials

--- tcc_reddit/src/main/java/com/example/tcc_reddit/controller/reddit/RedditApiController.java
+++ tcc_reddit/src/main/java/com/example/tcc_reddit/controller/reddit/RedditApiController.java
@@ -1,10 +1,49 @@
 package com.example.tcc_reddit.controller.reddit;
 
+import com.example.tcc_reddit.credentials.Credentials;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
-@RequestMapping("reddit-api")
+@RequestMapping("/reddit-api")
 public class RedditApiController {
 
+    private static String accessToken;
+
+    @Autowired
+    public RedditApiController(Credentials credentials){
+        accessToken = credentials.getAccessToken();
+    }
+
+    @GetMapping("/accessToken")
+    public String getMyAcessToken(){
+        if(accessToken != null){
+            return accessToken;
+        }else{
+            return "Erro ao recuperar o accessToken.";
+        }
+    }
+
+    @GetMapping("/meu-karma")
+    public String getMyKarma(){
+        RestTemplate restTemplate = new RestTemplate();
+        String url = "https://oauth.reddit.com/api/v1/me/karma";
+
+        HttpHeaders header = new HttpHeaders();
+        header.set("User-Agent", "tccBot");
+        header.set("Authorization", "Bearer" + accessToken);
+        HttpEntity<String> entity = new HttpEntity<>(header);
+
+        ResponseEntity<String> responce = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
+
+        if(responce.getStatusCode() == HttpStatus.OK){
+            return responce.getBody();
+        } else {
+            return "Erro ao recuperar o Karma do usuário.";
+        }
+    }
 }
--- tcc_reddit/src/main/java/com/example/tcc_reddit/credentials/Credentials.java
+++ tcc_reddit/src/main/java/com/example/tcc_reddit/credentials/Credentials.java
@@ -1,10 +1,14 @@
 package com.example.tcc_reddit.credentials;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
 import org.springframework.stereotype.Component;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
 import java.time.LocalDateTime;
+import java.util.Base64;
 
 @Component
 public class Credentials {
@@ -14,6 +18,12 @@ public class Credentials {
 
     @Value("${reddit.secret-id}")
     private String secret_id;
+
+    @Value("${reddit.username}")
+    private String username;
+
+    @Value("${reddit.password}")
+    private String password;
 
     private static AccessTokenResponce accessToken;
 
@@ -27,20 +37,48 @@ public class Credentials {
     }
 
     private boolean accessTokenAindaEhValido(){
+        if(accessToken == null){
+            return false;
+        }
         LocalDateTime currentTime = LocalDateTime.now();
         LocalDateTime expiracaoDoAccessToken = currentTime.plusSeconds(Long.parseLong(accessToken.getExpires_in()));
         return currentTime.isBefore(expiracaoDoAccessToken);
     }
 
     private void ObterAccessToken(){
+        String clientIdESecretId = this.cliend_id + ":" + this.secret_id;
+        String autorizacaoEnconded = Base64.getEncoder().encodeToString(clientIdESecretId.getBytes());
+
+        HttpHeaders header = new HttpHeaders();
+        header.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+        header.add("Authorization", "Basic " + autorizacaoEnconded);
+        header.set("User-Agent", "tccBot");
+
+        MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
+        body.add("grant_type", "password");
+        body.add("username", this.username);
+        body.add("password", this.password);
+
+        HttpEntity<MultiValueMap<String, String>> requestEntity = new HttpEntity<>(body, header);
+
         RestTemplate restTemplate = new RestTemplate();
+        String url = "https://www.reddit.com/api/v1/access_token";
+
+        ResponseEntity<AccessTokenResponce> response = restTemplate.exchange(url, HttpMethod.POST, requestEntity, AccessTokenResponce.class);
+        accessToken = response.getBody();
+    }
+
+    /*private void ObterAccessToken(){
+        RestTemplate restTemplate = new RestTemplate();sao
         String url_request = "https://www.reddit.com/api/v1/access_token";
         String grant_type = "password";
         String requestBody =
                 "grant_type=" + grant_type
                 + "&client_id=" + this.cliend_id
-                + "&client_secret" + this.secret_id;
+                + "&password" + this.secret_id;
         accessToken = restTemplate.postForObject(url_request, requestBody, AccessTokenResponce.class);
-    }
+    }*/
+
+
 
 }
