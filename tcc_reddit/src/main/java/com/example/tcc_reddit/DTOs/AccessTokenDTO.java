062478b922d9ef520f4b47a0ec3673e4f3ac3cf8
MoriHiroshi0619 <mori.hiroshi.a.c@gmail.com>
2024-04-28 19:11:40 -0400

    construindo metodo para recuperar posts mais recentes de um determinado subreddit

--- tcc_reddit/src/main/java/com/example/tcc_reddit/DTOs/AccessTokenDTO.java
+++ tcc_reddit/src/main/java/com/example/tcc_reddit/DTOs/AccessTokenDTO.java
@@ -1,4 +1,4 @@
-package com.example.tcc_reddit.credentials;
+package com.example.tcc_reddit.DTOs;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
--- tcc_reddit/src/main/java/com/example/tcc_reddit/controller/reddit/RedditApiController.java
+++ tcc_reddit/src/main/java/com/example/tcc_reddit/controller/reddit/RedditApiController.java
@@ -4,6 +4,7 @@ import com.example.tcc_reddit.credentials.Credentials;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -12,8 +13,12 @@ import org.springframework.web.client.RestTemplate;
 @RequestMapping("/reddit-api")
 public class RedditApiController extends BaseRedditController {
 
+    //@todo implementar tratamento de exceções (try catch)
+    //@todo deixar uma instancia de RestTemplate e HttpHeaders pronta no construtor, vai se repetir em todos os metodos
+    //@todo criar DIR de DTO's para serem salvos
     @Autowired
-    public RedditApiController(Credentials credentials){
+    public RedditApiController(Credentials credentials)
+    {
         super(credentials);
     }
 
@@ -26,7 +31,7 @@ public class RedditApiController extends BaseRedditController {
         }
     }
 
-    @GetMapping("/meu-karma")
+    @GetMapping("/my-karma")
     public String getMyKarma(){
         RestTemplate restTemplate = new RestTemplate();
         String url = getEndpoint(RedditEndpoint.KARMA);
@@ -45,4 +50,22 @@ public class RedditApiController extends BaseRedditController {
         }
     }
 
+    @GetMapping("/new-posts-from/{subreddit}")
+    public String getNewPostsFromSubreddit (@PathVariable("subreddit") String subreddit){
+        RestTemplate restTemplate = new RestTemplate();
+        String url = getEndpointPathWithSubreddit(RedditEndpoint.SUBREDDIT_NEW, subreddit);
+
+        HttpHeaders header = new HttpHeaders();
+        header.set("User-Agent", getUserAgent());
+        header.set("Authorization", getAccesstoken());
+        HttpEntity<String> headerEntity = new HttpEntity<>(header);
+
+        ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, headerEntity, String.class);
+
+        if(response.getStatusCode() == HttpStatus.OK){
+            return response.getBody();
+        }else{
+            return "Erro ao recuperar o posts desse Subreddit";
+        }
+    }
 }
