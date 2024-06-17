32e708a0a898a3e85d43bb72c81fd7dcf494e21b
Mori Hiroshi <mori.hiroshi.a.c@gmail.com>
2024-06-16 22:15:16 -0400

    feat: teste

--- tcc_reddit/src/main/java/com/example/tcc_reddit/repository/SubRedditPostRepository.java
+++ tcc_reddit/src/main/java/com/example/tcc_reddit/repository/SubRedditPostRepository.java
@@ -4,5 +4,5 @@ import com.example.tcc_reddit.model.SubRedditPost;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SubRedditPostRepository extends JpaRepository<SubRedditPost, String> {
-
+    //todo teste
 }
